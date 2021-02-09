300c2dd0cf7d1239ef68dc72947036285a714902
a0939946923 <a0939946923@gmail.com>
2021-02-10 04:41:25 +0800

    no message

---
+++ GattServetTest/build.sh
@@ -0,0 +1,4 @@
+g++ beacon.cc -c -lbluetooth
+g++ util.cc -c
+
+g++ Main.cpp -o test -I/home/pi/bleconfd/build/deps/src/bluez -lbluetooth /home/pi/bleconfd/build/deps/src/bluez/src/.libs/libshared-mainloop.a /home/pi/bleconfd/build/deps/src/bluez/lib/.libs/uuid.o beacon.o util.o
