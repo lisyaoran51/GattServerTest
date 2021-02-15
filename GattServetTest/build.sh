5309c2ced11eafb2df4406f348181c7a58e89a02
a0939946923 <a0939946923@gmail.com>
2021-02-16 02:15:09 +0800

    no message

--- GattServetTest/build.sh
+++ GattServetTest/build.sh
@@ -3,4 +3,4 @@ g++ util.cc -c
 
 #g++ Main.cpp -o test -pthread -I/home/pi/bleconfd/build/deps/src/bluez -lbluetooth /home/pi/bleconfd/build/deps/src/bluez/src/.libs/libshared-mainloop.a /home/pi/bleconfd/build/deps/src/bluez/lib/.libs/uuid.o beacon.o util.o
 
-g++ Main.cpp -o test -pthread -I~/bluez-5.43_test -lbluetooth ~/bluez-5.43_test/src/.libs/libshared-mainloop.a ~/bluez-5.43_test/lib/.libs/uuid.o beacon.o util.o
+g++ Main.cpp -o test -pthread -I/home/pi/bluez-5.43_test -lbluetooth /home/pi/bluez-5.43_test/src/.libs/libshared-mainloop.a /home/pi/bluez-5.43_test/lib/.libs/uuid.o beacon.o util.o
