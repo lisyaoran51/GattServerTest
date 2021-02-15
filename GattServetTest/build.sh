9a37e4e1670377d49b300ab22857e5d6d917176e
a0939946923 <a0939946923@gmail.com>
2021-02-16 02:09:55 +0800

    no message

--- GattServetTest/build.sh
+++ GattServetTest/build.sh
@@ -1,4 +1,6 @@
 g++ beacon.cc -c -lbluetooth
 g++ util.cc -c
 
-g++ Main.cpp -o test -pthread -I/home/pi/bleconfd/build/deps/src/bluez -lbluetooth /home/pi/bleconfd/build/deps/src/bluez/src/.libs/libshared-mainloop.a /home/pi/bleconfd/build/deps/src/bluez/lib/.libs/uuid.o beacon.o util.o
+#g++ Main.cpp -o test -pthread -I/home/pi/bleconfd/build/deps/src/bluez -lbluetooth /home/pi/bleconfd/build/deps/src/bluez/src/.libs/libshared-mainloop.a /home/pi/bleconfd/build/deps/src/bluez/lib/.libs/uuid.o beacon.o util.o
+
+g++ Main.cpp -o test -pthread -I~/bluez-5.43_test -lbluetooth ~/bluez-5.43_test/src/.libs/libshared-mainloop.a ~/bluez-5.43_test/lib/.libs/uuid.o beacon.o util.o
--- README.md
+++ README.md
@@ -5,3 +5,14 @@ https://github.com/gladish/bleconfd
 g++ Main.cpp -o test -I/home/pi/bleconfd/build/deps/src/bluez -lbluetooth /home/pi/bleconfd/build/deps/src/bluez/src/.libs/libshared-mainloop.a /home/pi/bleconfd/build/deps/src/bluez/lib/.libs/uuid.o
 
 sudo hciconfig hci0 leadv 0
+
+在att.c最後加一行
+int bt_att_write_queue_size(struct bt_att *att){
+	
+	return queue_length(att->write_queue);
+}
+
+att.h加一行
+int bt_att_write_queue_size(struct bt_att *att);
+
+然後重新compile bluez，再拿他的libshared-mainloop.a和uuid.o出來用
